d6c321d83e32f476ce4c8e1c0011ad403f41e7e4
AlbitaVM <albavm2003@gmail.com>
2025-10-13 12:25:29 +0200

    creacion proyecto spring

---
+++ src/main/java/es/albavm/tfg/tryfly/TryflyApplication.java
@@ -0,0 +1,13 @@
+package es.albavm.tfg.tryfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/albavm/tfg/tryfly/TryflyApplicationTests.java
@@ -0,0 +1,13 @@
+package es.albavm.tfg.tryfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
